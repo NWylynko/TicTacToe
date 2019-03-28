eeecb3090525b02be01b14d448b5ba10d8b8f31d
Nick Wylynko <nick1014375@gmail.com>
2019-03-27 21:45:37 -0400

    renamed and deleteded some files, made some changes on the server

--- learning networking/two way server.py
+++ learning networking/two way server.py
@@ -1,20 +1,22 @@
 import socket               # Import socket module
 
 print(socket.gethostname() + " is your ip")
+ip = input("enter host ip or leave blank to host ")
 
 localsocket = socket.socket()         # Create a socket object
 localhost = socket.gethostname() # Get local machine name
 localport = 11511                # Reserve a port for your service.
 localsocket.bind((localhost, localport))        # Bind to the port
 
-localsocket.listen(5)                 # Now wait for client connection.
+if ip == "":
+    localsocket.listen(5)                 # Now wait for client connection.
 while True:
    peerOut = localsocket.accept()     # Establish connection with client.
    print('Got connection from', addr)
    peerOut.send('Thank you for connecting')
 
    peersocket = socket.socket()
-   peerhost = addr[0]
+   peerhost = ip
    peerport = 11511
    peersocket.connect((peerhost, peerport))
 
